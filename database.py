322afa2e95c3ef417dce6570b23d1cd89e1dd795
Sourav Das <das361h@gmail.com>
2025-05-14 09:04:26 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql://root:GEyrFPejWzghAJcnaDPJMiaHqVjIxVsD@mysql.railway.internal:3306/railway"
+DATABASE_URL = "mysql://root:HhXaWMaflcZSjCPXteatdiYZlqSGnMMa@mysql.railway.internal:3306/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
